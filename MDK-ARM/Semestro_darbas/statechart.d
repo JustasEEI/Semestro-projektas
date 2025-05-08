e008260a2745c568b6a59d78c52e113be73cac93
JustasEEI <justasabraitis22@gmail.com>
2025-05-08 21:52:05 +0300

    Add files via upload

---
+++ MDK-ARM/Semestro_darbas/statechart.d
@@ -0,0 +1,5 @@
+semestro_darbas/statechart.o: \
+  ..\..\..\..\..\itemis_CREATE\ws\Semestro_darbas\src-gen\Statechart.c \
+  ..\..\..\..\..\itemis_CREATE\ws\Semestro_darbas\src-gen\..\src\sc_types.h \
+  ..\..\..\..\..\itemis_CREATE\ws\Semestro_darbas\src-gen\Statechart.h \
+  ..\..\..\..\..\itemis_CREATE\ws\Semestro_darbas\src-gen\Statechart_required.h
